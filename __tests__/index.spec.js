c78ac6ac01b8f2010e33c67e1d332ec9264eb3ce
huang.jian <hjj491229492@hotmail.com>
2018-10-25 18:09:37 +0800

    test: webpack generator UT.

---
+++ __tests__/index.spec.js
@@ -0,0 +1,35 @@
+/**
+ * @description - coco generator
+ * @author - huang.jian <hjj491229492@hotmail.com>
+ */
+
+// packages
+const path = require('path');
+const fs = require('fs');
+const renderer = require('yeoman-test');
+const assert = require('yeoman-assert');
+
+describe('coco:webpack', () => {
+  it('should generate projects with preset webpack', () => {
+    const webpack = path.resolve(__dirname, '../webpack');
+    const prompts = {
+      typescript: true,
+      cssmodules: false,
+      definition: 'bootcdn.stable.yml',
+      entry: './src/main.js',
+    };
+
+    return renderer
+      .run(webpack)
+      .withPrompts(prompts)
+      .then(() => {
+        assert.file(['webpack.development.js', 'webpack.production.js']);
+
+        const development = fs.readFileSync('webpack.development.js', 'utf8');
+        const production = fs.readFileSync('webpack.production.js', 'utf8');
+
+        expect(development).toMatchSnapshot();
+        expect(production).toMatchSnapshot();
+      });
+  });
+});
