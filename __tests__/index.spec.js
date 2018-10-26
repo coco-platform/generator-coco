54418a33f43a65f266e6b0ff1584f78d51fc6073
huang.jian <hjj491229492@hotmail.com>
2018-10-26 10:20:27 +0800

    fix: change plugins list when typescript active.

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -10,10 +10,33 @@ const renderer = require('yeoman-test');
 const assert = require('yeoman-assert');
 
 describe('coco:webpack', () => {
-  it('should generate projects with preset webpack', () => {
+  it('should generate webpack configuration with typescript', () => {
     const webpack = path.resolve(__dirname, '../webpack');
     const prompts = {
       typescript: true,
+      cssmodules: false,
+      definition: 'bootcdn.stable.yml',
+      entry: './src/main.js',
+    };
+
+    return renderer
+      .run(webpack)
+      .withPrompts(prompts)
+      .then(() => {
+        assert.file(['webpack.development.js', 'webpack.production.js']);
+
+        const development = fs.readFileSync('webpack.development.js', 'utf8');
+        const production = fs.readFileSync('webpack.production.js', 'utf8');
+
+        expect(development).toMatchSnapshot();
+        expect(production).toMatchSnapshot();
+      });
+  });
+
+  it('should generate webpack configuration without typescript', () => {
+    const webpack = path.resolve(__dirname, '../webpack');
+    const prompts = {
+      typescript: false,
       cssmodules: false,
       definition: 'bootcdn.stable.yml',
       entry: './src/main.js',
