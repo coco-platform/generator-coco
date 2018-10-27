bf04e26ea7431b8e3ed2b4bd9531a277fcb82742
huang.jian <hjj491229492@hotmail.com>
2018-10-27 22:02:24 +0800

    feature: jest generator.

---
+++ __tests__/jest.spec.js
@@ -0,0 +1,59 @@
+/**
+ * @description - coco:babel generator
+ * @author - huang.jian <hjj491229492@hotmail.com>
+ */
+
+// packages
+const path = require('path');
+const fs = require('fs');
+const yeomon = require('yeoman-test');
+const assert = require('yeoman-assert');
+const hbs = require('handlebars');
+
+describe('coco:jest generator', () => {
+  const template = fs.readFileSync(
+    path.resolve(process.cwd(), 'jest', 'templates', 'jest.config.js.hbs'),
+    'utf8'
+  );
+  const compile = hbs.compile(template);
+
+  it('should properly render template with universal require', () => {
+    const context = {
+      universal: true,
+      root: 'native',
+      environment: 'node',
+    };
+
+    expect(compile(context)).toMatchSnapshot();
+  });
+
+  it('should properly render template without universal require', () => {
+    const context = {
+      universal: false,
+      root: 'native',
+      environment: 'node',
+    };
+
+    expect(compile(context)).toMatchSnapshot();
+  });
+
+  it('should output jest configuration', () => {
+    const jest = path.resolve(__dirname, '../jest');
+    const prompts = {
+      universal: false,
+      environment: 'node',
+      root: './src',
+    };
+
+    return yeomon
+      .run(jest)
+      .withPrompts(prompts)
+      .then(() => {
+        assert.file(['jest.config.js']);
+
+        const configuration = fs.readFileSync('jest.config.js', 'utf8');
+
+        expect(configuration).toMatchSnapshot();
+      });
+  });
+});
---
+++ jest/index.js
@@ -0,0 +1,64 @@
+/**
+ * @description -coco travis generator
+ * @author - huang.jian <hjj491229492@hotmail.com>
+ */
+
+/**
+ * @typedef {object} JestAnswers
+ *
+ * @property {boolean} universal - whether require none javascript assets
+ * @property {string} environment - node or jsdom
+ * @property {string} root - source code root directory
+ */
+
+// packages
+const fs = require('fs');
+const hbs = require('handlebars');
+const Generator = require('yeoman-generator');
+
+class JestGenerator extends Generator {
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: 'confirm',
+        name: 'universal',
+        message: 'Do you need require none javascript assets?',
+        default: false,
+      },
+      {
+        type: 'list',
+        name: 'environment',
+        message: 'What environment do you need?',
+        choices: ['node', 'jsdom'],
+        default: 0,
+      },
+      {
+        type: 'input',
+        name: 'root',
+        message: `What's your source code directory name?`,
+        default: 'src',
+      },
+    ]);
+  }
+
+  configuring() {
+    const template = fs.readFileSync(
+      this.templatePath('jest.config.js.hbs'),
+      'utf8'
+    );
+    const compile = hbs.compile(template);
+    const content = compile(this.answers);
+
+    this.fs.write(this.destinationPath('jest.config.js'), content);
+  }
+
+  install() {
+    const devDependencies = !this.answers.universal
+      ? ['babel-jest']
+      : ['babel-jest', '@coco-platform/jest-tools'];
+
+    this.yarnInstall(devDependencies, { dev: true });
+  }
+}
+
+module.exports = JestGenerator;
