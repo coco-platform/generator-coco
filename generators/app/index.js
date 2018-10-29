0942b876e87ebe2bea9fe1363aa65990b24eecc6
huang.jian <hjj491229492@hotmail.com>
2018-10-29 18:00:51 +0800

    optimize: split generators by prompts.

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,15 +14,19 @@ const prettier = require.resolve('../prettier');
 const travis = require.resolve('../travis');
 const typescript = require.resolve('../typescript');
 const webpack = require.resolve('../webpack');
+const editor = require.resolve('../editor');
 
 class Coco extends Generator {
   initializing() {
-    this.composeWith(babel, {});
-    this.composeWith(eslint, {});
+    // generator without prompts
+    this.composeWith(editor, {});
     this.composeWith(git, {});
-    this.composeWith(jest, {});
     this.composeWith(prettier, {});
     this.composeWith(travis, {});
+    // generator with prompts
+    this.composeWith(babel, {});
+    this.composeWith(eslint, {});
+    this.composeWith(jest, {});
     this.composeWith(typescript, {});
     this.composeWith(webpack, {});
   }
