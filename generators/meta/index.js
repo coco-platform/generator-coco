4230600cff48257470e7199febc2b0f5614438ec
huang.jian <hjj491229492@hotmail.com>
2018-10-30 15:10:01 +0800

    feature: implement meta generator.

--- generators/meta/index.js
+++ generators/meta/index.js
@@ -3,16 +3,67 @@
  * @author - huang.jian <hjj491229492@hotmail.com>
  */
 
+/* eslint-disable indent,max-len */
+
 // packages
 const hbs = require('handlebars');
 const Generator = require('yeoman-generator');
 // scope
 const regExp = {
   scope: /^[\w-]+$/,
-  author: /^[a-z]{3,8}\.?[a-z]{3,20}$/,
+};
+const PresetScripts = {
+  Node: [
+    { key: 'clean', value: 'rm -rf native es lib' },
+    { key: 'compile', value: 'npm-run-all -s compile:*' },
+    { key: 'compile:native', value: 'tsc --declaration --outDir native' },
+    {
+      key: 'compile:cjs',
+      value: `babel src --source-maps --out-dir es --extensions '.js,.ts'`,
+    },
+  ],
+  Web: [
+    { key: 'clean', value: 'rm -rf native es lib' },
+    { key: 'compile', value: 'npm-run-all -s compile:*' },
+    { key: 'compile:native', value: 'tsc --declaration --outDir native' },
+    {
+      key: 'compile:esm',
+      value: `babel src --source-maps --out-dir es --extensions '.js,.ts'`,
+    },
+    {
+      key: 'compile:cjs',
+      value: `babel src --source-maps --out-dir lib --extensions '.js,.ts' --plugins=@babel/plugin-transform-modules-commonjs`,
+    },
+    {
+      key: 'dev',
+      value: 'NODE_ENV=development webpack-dev-server --hot --inline',
+    },
+  ],
+  React: [
+    { key: 'clean', value: 'rm -rf dist' },
+    {
+      key: 'dev',
+      value: 'NODE_ENV=development webpack-dev-server --hot --inline',
+    },
+    {
+      key: 'build',
+      value: 'NODE_ENV=production webpack --config webpack.production.js',
+    },
+  ],
 };
 
 class MetaGenerator extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.option('environment', {
+      desc: 'the runtime environment, enum React, Node, Web',
+      alias: 'e',
+      type: String,
+      default: 'Node',
+    });
+  }
+
   async prompting() {
     this.answers = await this.prompt([
       {
@@ -53,13 +104,26 @@ class MetaGenerator extends Generator {
   }
 
   writing() {
-    const readmeTemplate = this.fs.read(this.templatePath('README.md.hbs'));
-    const readmeContent = hbs.compile(readmeTemplate)(this.answers);
-    const jsonTemplate = this.fs.read(this.templatePath('package.json.hbs'));
-    const jsonContent = hbs.compile(jsonTemplate)(this.answers);
+    const readme = {
+      template: this.fs.read(this.templatePath('README.md.hbs')),
+      context: this.answers,
+    };
+    const json = {
+      template: this.fs.read(this.templatePath('package.json.hbs')),
+      context: {
+        ...this.answers,
+        scripts: Reflect.get(PresetScripts, this.options.environment),
+      },
+    };
 
-    this.fs.write(this.destinationPath('README.md'), readmeContent);
-    this.fs.write(this.destinationPath('package.json'), jsonContent);
+    this.fs.write(
+      this.destinationPath('README.md'),
+      hbs.compile(readme.template)(readme.context)
+    );
+    this.fs.write(
+      this.destinationPath('package.json'),
+      hbs.compile(json.template)(json.context)
+    );
   }
 }
 
