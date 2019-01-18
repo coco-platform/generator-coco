f29967015e45652e89d9340561ab147c5a18ad0f
huang.jian <hjj491229492@hotmail.com>
2019-01-18 11:07:41 +0800

    optimize(prettier): add pretty hook with husky.

--- generators/prettier/index.js
+++ generators/prettier/index.js
@@ -4,10 +4,30 @@
  */
 
 // packages
+const merge = require('deepmerge');
 const Generator = require('yeoman-generator');
 
 class PrettierGenerator extends Generator {
   writing() {
+    const metaFile = this.destinationPath('package.json');
+    const exist = this.fs.exists(metaFile);
+
+    // add prettier hook
+    if (exist) {
+      const meta = this.fs.readJSON(metaFile);
+      const previous = meta.husky || {};
+      const extension = {
+        hooks: {
+          'pre-commit': 'pretty-quick --staged',
+        },
+      };
+      const override = {
+        husky: merge(previous, extension),
+      };
+
+      this.fs.extendJSON(metaFile, override, null, 2);
+    }
+
     this.fs.copy(
       this.templatePath('.prettierrc'),
       this.destinationPath('.prettierrc')
@@ -15,7 +35,7 @@ class PrettierGenerator extends Generator {
   }
 
   install() {
-    const devDependencies = ['prettier', 'pretty-quick'];
+    const devDependencies = ['prettier', 'pretty-quick', 'husky'];
 
     this.yarnInstall(devDependencies, { dev: true });
   }
