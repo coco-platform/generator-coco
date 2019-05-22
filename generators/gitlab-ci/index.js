2dd2c4fea6c098bf35f28673169cebe1094a63a6
huang.jian <hjj491229492@hotmail.com>
2019-05-22 15:37:50 +0800

    refactor: prepare for nodejs, java project

--- generators/gitlab-ci/index.js
+++ generators/gitlab-ci/index.js
@@ -4,13 +4,53 @@
  */
 
 // packages
+const chalk = require('chalk');
+const hbs = require('handlebars');
 const Generator = require('yeoman-generator');
 
 class GitlabCIGenerator extends Generator {
+  async prompting() {
+    const projects = [
+      {
+        type: 'rawlist',
+        name: 'project',
+        message: `What's the project type?`,
+        choices: ['web', 'nodejs', 'java'],
+        default: 0,
+      },
+    ];
+
+    const environments = [
+      {
+        type: 'confirm',
+        name: 'scale',
+        message: `Do you have more than one production cluster?`,
+        default: true,
+      },
+    ];
+
+    const project = await this.prompt(projects);
+    const environment = await this.prompt(environments);
+
+    this.answers = { ...project, ...environment };
+  }
+
   configuring() {
-    this.fs.copy(
-      this.templatePath('.gitlab-ci.yml'),
-      this.destinationPath('.gitlab-ci.yml')
+    if (this.answers.project === 'web') {
+      const template = this.fs.read(this.templatePath('web.yml.hbs'));
+      const compile = hbs.compile(template);
+      const destiny = this.destinationPath('.gitlab-ci.yml');
+      const content = compile(this.answers);
+
+      this.fs.write(destiny, content);
+
+      return;
+    }
+
+    this.log(
+      chalk.cyan(
+        ` Todo - Gitlab CI for ${this.answers.project} is still in progress!!!`
+      )
     );
   }
 }
